4540c3d89a829c530454f72c4ecbab2ba97cf0f1
Довбан Андрей Сергеевич <dovban@mdis.ru>
2025-09-10 11:18:49 +0300

    fix

--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	tr := make([]int, 0, 2)
+	tr = append(tr, 1)
+	tr = append(tr, 2)
+	tr[4] = 100
+	fmt.Println(tr)
+
+	transaction := []int{}
+
+	for {
+		q := entryTransaction(&transaction)
+		if !q {
+			break
+		}
+	}
+
+	fmt.Println(transaction)
+	sum := getSumTransaction(&transaction)
+
+	fmt.Println(sum)
+
+}
+
+func entryTransaction(trs *[]int) bool {
+	var transaction int
+	var questoins string
+
+	fmt.Println("Введите трансакцию:")
+	fmt.Scan(&transaction)
+
+	*trs = append(*trs, transaction)
+
+	fmt.Println("Продолжить?")
+	fmt.Scan(&questoins)
+
+	if strings.ToLower(questoins) == "y" || strings.ToLower(questoins) == "yes" {
+		return true
+	}
+	return false
+}
+
+func getSumTransaction(trs *[]int) int {
+	var sum int
+	for _, el := range *trs {
+		sum += el
+	}
+	return sum
 }
